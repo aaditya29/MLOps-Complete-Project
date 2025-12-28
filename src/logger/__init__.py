2a0194c346c668fb382c8f3b93deafd12b497e06
Aditya Mishra <mishraaaditya29@gmail.com>
2025-12-29 00:40:46 +0530

    refactor logger configuration for improved structure and clarity

--- src/logger/__init__.py
+++ src/logger/__init__.py
@@ -3,3 +3,46 @@ import sys
 import logging
 from datetime import datetime
 from logging.handlers import RotatingFileHandler  # for log rotation
+
+LOG_DIR = 'logs'
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+MAX_LOG_SIZE = 5 * 1024 * 1024  # 5 MB
+BACKUP_COUNT = 3  # Number of backup log files to keep
+
+# Construct log file path
+root_dir = os.path.dirname(os.path.abspath(
+    # Get the root directory of the project
+    os.path.join(os.path.dirname(__file__), '../')))
+log_dir_path = os.path.join(root_dir, LOG_DIR)  # Ensure log directory exists
+os.makedirs(log_dir_path, exist_ok=True)  # Construct full log file path
+# Function to configure logger
+log_file_path = os.path.join(log_dir_path, LOG_FILE)
+
+
+def configure_logger():
+    # Create a custom logger
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)  # Set the minimum logging level
+
+    # Define formatter
+    formatter = logging.Formatter(
+        "[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s")
+
+    # File handler with rotation
+    file_handler = RotatingFileHandler(
+        log_file_path, maxBytes=MAX_LOG_SIZE, backupCount=BACKUP_COUNT, encoding="utf-8")
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.INFO)
+
+    # Console handler
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(formatter)
+    console_handler.setLevel(logging.INFO)
+
+    # Add handlers to the logger
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+
+# Configure the logger
+configure_logger()
